e0121cb6f16764178322e4bb73724cf95c07508d
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2024-05-23 20:17:58 +0530

    something

--- frontend/src/Apis/userApi.js
+++ frontend/src/Apis/userApi.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000/' })
+const API = axios.create({
+	baseURL: 'https://smartshopping-27iu.onrender.com/'
+})
 
 // const API = axios.create({ baseURL: 'https://account-back-4krv.onrender.com' })
 
